8bd8278351ed47a681e334c38c2cb12a2ea37a60
DhruvKalbalia <64431395+DhruvKalbalia@users.noreply.github.com>
2026-07-07 17:27:26 +0530

    Time: 0 ms (100%), Space: 14.7 MB (54.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> mp;
+        unordered_map<int,int> mp;//hashmap soln
 
         for (int i=0;i<nums.size();i++){
-            int complement = target-nums[i];
+            int complement = target-nums[i];//stores complement
 
-            if (mp.count(complement))
-                return {mp[complement],i};
+            if (mp.count(complement))//if complement is present 
+                return {mp[complement],i};//returns indices
 
             mp[nums[i]] = i;
         }
